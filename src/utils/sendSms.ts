1aa4cb5d8e406a40f4f7ba94ce8928f31ebad8bc
futurefocusit <ffocusit@gmail.com>
2025-05-22 15:46:36 +0200

    remove itec sms

--- src/utils/sendSms.ts
+++ src/utils/sendSms.ts
@@ -11,16 +11,16 @@ export interface smsInterface {
 export const sendMessage = async (message: string, recipients: [string]) => {
   const api_key = process.env.SMS_API_KEY as string;
   try {
-    const response = await axios.post("https://itecsms.rw/api/sendsms", {
-      key: api_key,
-      message,
-      recipients
-    }, {
-      headers: {
-        "Content-Type": "application/json",
-      }
+    // const response = await axios.post("https://itecsms.rw/api/sendsms", {
+    //   key: api_key,
+    //   message,
+    //   recipients
+    // }, {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   }
       
-    });
+    // });
 
     const responseData = response.data;
     const status = responseData.status;
