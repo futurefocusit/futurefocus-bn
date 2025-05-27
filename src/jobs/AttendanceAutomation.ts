ef2a3e35fc987ff4f2bec198ec1290c0fa710882
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2025-05-27 08:22:46 +0200

    corrrect week day

--- src/jobs/AttendanceAutomation.ts
+++ src/jobs/AttendanceAutomation.ts
@@ -27,7 +27,7 @@ export const dailyAttendance = () => {
   });
 };
 export const teamAttendance = () => {
-  cron.schedule("00 05 * * 1-8", async () => {
+  cron.schedule("00 05 * * 1-6", async () => {
     try {
       const members = await Team.find({active:true,attend:true});
       for (const member of members) {
